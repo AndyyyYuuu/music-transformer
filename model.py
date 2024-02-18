3b78f4b9ece9477972b81aab0a9eff096469a48e
Andy <77585612+AndyyyYuuu@users.noreply.github.com>
2024-02-17 18:02:35 -0800

    Add basic LSTM architecture

--- model.py
+++ model.py
@@ -1,2 +1,19 @@
 import torch
 import numpy as np
+
+class Composer(torch.nn.Module):
+    def __init__(self, num_notes):
+        super().__init__()
+
+        self.lstm = torch.nn.LSTM(input_size=1, hidden_size=256, num_layers=3, batch_first=True, dropout=0.2)
+
+        self.dropout = torch.nn.Dropout(0.2)
+        self.linear = torch.nn.Linear(256, num_notes)
+
+    def forward(self, x):
+        x, _ = self.lstm(x)
+        # take only the last output
+        x = x[:, -1, :]
+        # produce output
+        x = self.linear(self.dropout(x))
+        return x
