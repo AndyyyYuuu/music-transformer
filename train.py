e8a080e6721f66bd29ea91801d22b44b9d8ddf68
Andy <77585612+AndyyyYuuu@users.noreply.github.com>
2024-02-22 14:01:36 -0800

    Add validation and saving

--- train.py
+++ train.py
@@ -1,16 +1,22 @@
 import torch
+import os
 import numpy as np
 import wandb
-
 
 from midi_processor import processor
 import model
 import datasets
 import utils
 
-NUM_EPOCHS = 10
+NUM_EPOCHS = 5
 TRAIN_SPLIT = 0.8
 SEQ_LENGTH = 100
+
+SAVE_PATH = "models/jazz-1.pth"
+
+def checkpoint(best_model, best_loss, epoch):
+    torch.save([best_model, best_loss, epoch], SAVE_PATH)
+
 
 
 print("Loading dataset...")
@@ -33,7 +39,33 @@ composer = model.Composer(dataset.vocab)
 optimizer = torch.optim.Adam(composer.parameters())
 loss_function = torch.nn.CrossEntropyLoss(reduction="sum")
 
+best_model = None
+best_loss = np.inf
+
+# Load checkpoint
+if os.path.exists(SAVE_PATH):
+    past_state_dict = torch.load(SAVE_PATH)
+    loaded_best_model, loaded_best_loss, loaded_epoch = past_state_dict
+    if loaded_epoch < NUM_EPOCHS-1:
+        best_model = loaded_best_model
+        best_loss = loaded_best_loss
+        start_epoch = loaded_epoch+1
+        print("LOADED MODEL")
+        print(f"Epochs to train: {NUM_EPOCHS-start_epoch}")
+        print(f"Save path: {SAVE_PATH}")
+        input("Enter to resume training >>> ")
+    else:
+        print(f"Hey Andy, this model is already trained up to {NUM_EPOCHS} epochs.")
+        exit(0)
+else:
+    print("TRAIN NEW MODEL")
+    print(f"Epochs to train: {NUM_EPOCHS}")
+    print(f"Save path: {SAVE_PATH}")
+
+
+
 for epoch in range(NUM_EPOCHS):
+    dataset.shuffle()
     composer.train()
     loading_iter = iter(train_loader)
     for i in utils.progress_iter(range(len(train_loader)), "Training"):
@@ -44,5 +76,21 @@ for epoch in range(NUM_EPOCHS):
         optimizer.zero_grad()
         loss.backward()
         optimizer.step()
-    print(y_pred)
+
+
+
+    composer.eval()
+    loss = 0
+    with torch.no_grad():
+        loading_iter = iter(test_loader)
+        for i in utils.progress_iter(test_loader, "Validating"):
+            X_batch, y_batch = next(loading_iter)
+            # X_batch, y_batch = X_batch.to(device), y_batch.to(device)
+            y_pred = composer(X_batch)
+            loss += loss_function(y_pred, y_batch)
+        if loss < best_loss:
+            best_loss = loss
+            best_model = composer.state_dict()
+        print(f"Loss: {loss}")
+        checkpoint(best_model, best_loss, epoch)
 
